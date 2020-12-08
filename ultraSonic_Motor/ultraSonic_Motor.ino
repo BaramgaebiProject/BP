b5d82ec911de4901efa5876a50e58cfa79de07a6
JineyKim <72438246+JineyKim@users.noreply.github.com>
2020-12-09 00:35:48 +0900

    2012091235

--- ultraSonic_Motor/ultraSonic_Motor.ino
+++ ultraSonic_Motor/ultraSonic_Motor.ino
@@ -1,4 +1,4 @@
-void sampling(){
+int sampling(){
   digitalWrite(8,0);
   digitalWrite(9,0);
   delayMicroseconds(2);
@@ -8,8 +8,8 @@ void sampling(){
   unsigned long duration=pulseIn(9,1);
   float distance=duration/5.8;
   int distint=int(distance);
-  Serial.println(distint);
-  return distint;
+  Serial.println(distance);
+  return int(distance);
 }
 void dispenser(){
   digitalWrite(4,0);
@@ -19,6 +19,17 @@ void dispenser(){
   digitalWrite(4,0);
   digitalWrite(5,0);
   analogWrite(3,0);
+  delay(10);
+}
+void inputPaper(){
+  digitalWrite(4,1);
+  digitalWrite(5,0);
+  analogWrite(3,255);
+  delay(500);
+  digitalWrite(4,0);
+  digitalWrite(5,0);
+  analogWrite(3,0);
+  delay(10);
 }
 void setup(){  
   Serial.begin(9600);
@@ -28,8 +39,10 @@ void setup(){
   pinMode(9,OUTPUT);
 }
 void loop(){
-  int sample=sampling();
+  int distint=sampling();
   delay(10);
   dispenser();
+  delay(10);
+  inputPaper();
   delay(10);
 }
